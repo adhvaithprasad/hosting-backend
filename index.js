ac1d416e1a4d8c49549e4bb7ef738f16d1f6a781
Adhvaith Prasad <83104991+firescrypt@users.noreply.github.com>
2022-01-24 07:15:06 +0000

    added eslint

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
-var pushover = require('pushover');
-var repos = pushover('/tmp/repos');
-var express = require('express');
+const pushover = require('pushover');
+const repos = pushover('/tmp/repos');
+const express = require('express');
 const path = require('path');
-repos.on('push', function (push) {
-    console.log('push ' + push.repo + '/' + push.commit
-        + ' (' + push.branch + ')'
-    );
-    push.accept();
+repos.on('push', function(push) {
+  console.log('push ' + push.repo + '/' + push.commit +
+    ' (' + push.branch + ')',
+  );
+  push.accept();
 });
 
-repos.on('fetch', function (fetch) {
-    console.log('fetch ' + fetch.repo + '/' + fetch.commit);
-    fetch.accept();
+repos.on('fetch', function(fetch) {
+  console.log('fetch ' + fetch.repo + '/' + fetch.commit);
+  fetch.accept();
 });
 
 function authentication(req, res, next) {
-    var authheader = req.headers.authorization;
-    console.log(req.headers);
- 
-    if (!authheader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err)
-    }
- 
-    var auth = new Buffer.from(authheader.split(' ')[1],
-    'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
- 
-    if (user == 'a4' && pass == '123') {
- 
-        // If Authorized user
-        next();
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err);
-    }
- 
-}
-var app = express()
-.use('/console',express.static(__dirname + '/frontend'))
-.use('/git',authentication)
+  const authheader = req.headers.authorization;
+  console.log(req.headers);
 
-.use('/git',function(req, res, next){
-       
-        repos.handle(req, res);
-        
-    })
-    .listen(7005);
+  if (!authheader) {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  const auth = new Buffer.from(authheader.split(' ')[1], 
+      'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+
+  if (user == 'a4' && pass == '123') {
+    // If Authorized user
+    next();
+  } else {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+const app = express();
+app.use('/console', express.static(__dirname + '/frontend'));
+app.use('/git', authentication);
+app.use('/git', function(req, res, next) {
+  repos.handle(req, res);
+});
+app.listen(7005);
 
 
