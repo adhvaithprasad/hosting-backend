a131345519edb49c47638b49ee6791686bcbfd04
AdhvaithPrasad <83104991+firescrypt@users.noreply.github.com>
2022-01-24 06:10:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var pushover = require('pushover');
+var repos = pushover('/tmp/repos');
+var express = require('express');
+repos.on('push', function (push) {
+    console.log('push ' + push.repo + '/' + push.commit
+        + ' (' + push.branch + ')'
+    );
+    push.accept();
+});
+
+repos.on('fetch', function (fetch) {
+    console.log('fetch ' + fetch.repo + '/' + fetch.commit);
+    fetch.accept();
+});
+
+function authentication(req, res, next) {
+    var authheader = req.headers.authorization;
+    console.log(req.headers);
+ 
+    if (!authheader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err)
+    }
+ 
+    var auth = new Buffer.from(authheader.split(' ')[1],
+    'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+ 
+    if (user == 'a4' && pass == '123') {
+ 
+        // If Authorized user
+        next();
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+ 
+}
+var app = express()
+.get('/', function (req, res) {
+  res.send('about')
+})
+.use('/git',authentication)
+
+.use('/git',function(req, res, next){
+       
+        repos.handle(req, res);
+        
+    })
+    .listen(7005);
+
+
